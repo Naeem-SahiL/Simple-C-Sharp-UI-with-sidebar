2a2988485d9e95c12e1143100e184de2a6113ece
Naeem-SahiL <sahilsahb3630@gmail.com>
2022-02-18 18:40:37 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace my_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonTasks = new System.Windows.Forms.Button();
+            this.buttonCalender = new System.Windows.Forms.Button();
+            this.buttonSeller = new System.Windows.Forms.Button();
+            this.buttondashboard = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panelleft = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonTasks);
+            this.panel1.Controls.Add(this.buttonCalender);
+            this.panel1.Controls.Add(this.buttonSeller);
+            this.panel1.Controls.Add(this.buttondashboard);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(158, 531);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonTasks
+            // 
+            this.buttonTasks.FlatAppearance.BorderSize = 0;
+            this.buttonTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTasks.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonTasks.ForeColor = System.Drawing.Color.White;
+            this.buttonTasks.Image = ((System.Drawing.Image)(resources.GetObject("buttonTasks.Image")));
+            this.buttonTasks.Location = new System.Drawing.Point(0, 350);
+            this.buttonTasks.Name = "buttonTasks";
+            this.buttonTasks.Size = new System.Drawing.Size(158, 76);
+            this.buttonTasks.TabIndex = 1;
+            this.buttonTasks.Text = "Tasks";
+            this.buttonTasks.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonTasks.UseVisualStyleBackColor = true;
+            this.buttonTasks.Click += new System.EventHandler(this.buttonTasks_Click);
+            // 
+            // buttonCalender
+            // 
+            this.buttonCalender.FlatAppearance.BorderSize = 0;
+            this.buttonCalender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCalender.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCalender.ForeColor = System.Drawing.Color.White;
+            this.buttonCalender.Image = ((System.Drawing.Image)(resources.GetObject("buttonCalender.Image")));
+            this.buttonCalender.Location = new System.Drawing.Point(0, 268);
+            this.buttonCalender.Name = "buttonCalender";
+            this.buttonCalender.Size = new System.Drawing.Size(158, 76);
+            this.buttonCalender.TabIndex = 1;
+            this.buttonCalender.Text = "Calender";
+            this.buttonCalender.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonCalender.UseVisualStyleBackColor = true;
+            this.buttonCalender.Click += new System.EventHandler(this.buttonCalender_Click);
+            // 
+            // buttonSeller
+            // 
+            this.buttonSeller.FlatAppearance.BorderSize = 0;
+            this.buttonSeller.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSeller.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonSeller.ForeColor = System.Drawing.Color.White;
+            this.buttonSeller.Image = ((System.Drawing.Image)(resources.GetObject("buttonSeller.Image")));
+            this.buttonSeller.Location = new System.Drawing.Point(0, 186);
+            this.buttonSeller.Name = "buttonSeller";
+            this.buttonSeller.Size = new System.Drawing.Size(158, 76);
+            this.buttonSeller.TabIndex = 1;
+            this.buttonSeller.Text = "Sellers";
+            this.buttonSeller.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonSeller.UseVisualStyleBackColor = true;
+            this.buttonSeller.Click += new System.EventHandler(this.buttonSeller_Click);
+            // 
+            // buttondashboard
+            // 
+            this.buttondashboard.FlatAppearance.BorderSize = 0;
+            this.buttondashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttondashboard.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttondashboard.ForeColor = System.Drawing.Color.White;
+            this.buttondashboard.Image = ((System.Drawing.Image)(resources.GetObject("buttondashboard.Image")));
+            this.buttondashboard.Location = new System.Drawing.Point(0, 104);
+            this.buttondashboard.Name = "buttondashboard";
+            this.buttondashboard.Size = new System.Drawing.Size(158, 76);
+            this.buttondashboard.TabIndex = 1;
+            this.buttondashboard.Text = "Dashboard";
+            this.buttondashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttondashboard.UseVisualStyleBackColor = true;
+            this.buttondashboard.Click += new System.EventHandler(this.buttondashboard_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(158, 79);
+            this.panel3.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(25, -25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 117);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "m";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBox4);
+            this.panel2.Controls.Add(this.checkBox3);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(158, 321);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(602, 210);
+            this.panel2.TabIndex = 0;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(291, 159);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(87, 25);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Sleep....";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(291, 125);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(141, 25);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "SQL Database\r\n";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(291, 91);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(283, 25);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Data Structures Implementation\r\n";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(291, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 25);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "C# UI Design";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(264, 188);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(282, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tasks";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Sales";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(192, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 231);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(660, 30);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 29);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Week";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(575, 30);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 29);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Month";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(478, 30);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(91, 29);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Six Month";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(393, 30);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(79, 29);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Year";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // panelleft
+            // 
+            this.panelleft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panelleft.Location = new System.Drawing.Point(158, 104);
+            this.panelleft.Name = "panelleft";
+            this.panelleft.Size = new System.Drawing.Size(7, 65);
+            this.panelleft.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(760, 531);
+            this.Controls.Add(this.panelleft);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(130)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonTasks;
+        private System.Windows.Forms.Button buttonCalender;
+        private System.Windows.Forms.Button buttonSeller;
+        private System.Windows.Forms.Button buttondashboard;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panelleft;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttondashboard_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = buttondashboard.Height;
+            panelleft.Top = buttondashboard.Top;
+        }
+
+        private void buttonSeller_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = buttonSeller.Height;
+            panelleft.Top = buttonSeller.Top;
+        }
+
+        private void buttonCalender_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = buttonCalender.Height;
+            panelleft.Top = buttonCalender.Top;
+        }
+
+        private void buttonTasks_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = buttonTasks.Height;
+            panelleft.Top = buttonTasks.Top;
+        }
+    }
+}
